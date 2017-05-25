2f6e6fff8d2ab1bb57fb77d505e6276b834634fd
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-05-25 23:09:51 +0700

    add first file

---
+++ demo/file.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 100; i++ {
+		filename := fmt.Sprintf("%v.test.txt", i)
+		fmt.Println("writing: " + filename)
+
+		f, err := os.Create(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		for i := 0; i < 10; i++ {
+			fmt.Println(filename, ":", i)
+			c := fmt.Sprintf("%v ", i)
+			io.WriteString(f, c)
+			time.Sleep(time.Microsecond * 100)
+		}
+	}
+	var input string
+	fmt.Scanln(&input)
+}
